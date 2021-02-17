bef50b75002729a62a81be07c42fd6634db923c0
congon4tor <ignacio.dominguez95@gmail.com>
2021-02-17 19:34:49 +0100

    Added warning to request a CTF page and fixed gitignore

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,48 +1,49 @@
-import React from 'react'
-import Typewriter from 'typewriter-effect';
-import styled from 'styled-components'
+import React from "react"
+import Typewriter from "typewriter-effect"
+import styled from "styled-components"
 
 const Styles = styled.div`
-    font-family: monospace;
-    font-size: 35px;
-    color: #18d26e;
+  font-family: monospace;
+  font-size: 35px;
+  color: #18d26e;
 
-    .box{
-        padding: 50px 30px;
-        background: rgba(0, 0, 0, 0.9);
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%,-50%);
-        width: 80%;
-    }
-    
-    .Typewriter{
-        width:100%;
-    }
+  .box {
+    padding: 50px 30px;
+    background: rgba(0, 0, 0, 0.9);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+  }
 
-    .Typewriter__cursor{
-        font-size: 32px;
-        vertical-align: top;
-    }
+  .Typewriter {
+    width: 100%;
+  }
+
+  .Typewriter__cursor {
+    font-size: 32px;
+    vertical-align: top;
+  }
 `
 
 export const NotFound = () => (
-    <React.Fragment>
-        <Styles>
-            <div className="box">
-                <Typewriter
-                    options={{
-                        delay: 200,
-                        cursor: "&#9610;",
-                    }}
-                    onInit={(typewriter) => {
-                        typewriter.pauseFor(1500)
-                            .typeString('<strong>404</strong>, page not found.')
-                            .start()
-                    }}
-                />
-            </div>
-        </Styles>
-    </React.Fragment >
+  <React.Fragment>
+    <Styles>
+      <div className="box">
+        <Typewriter
+          options={{
+            delay: 200,
+            cursor: "&#9610;",
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .pauseFor(500)
+              .typeString("<strong>404</strong>, page not found.")
+              .start()
+          }}
+        />
+      </div>
+    </Styles>
+  </React.Fragment>
 )
--- src/pages/RequestCTF.js
+++ src/pages/RequestCTF.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import { SectionPanel } from '../components/SectionPanel'
-import { SectionTitle } from '../components/SectionTitle'
-import { Section } from '../components/Section'
+import React from "react"
+import { SectionPanel } from "../components/SectionPanel"
+import { SectionTitle } from "../components/SectionTitle"
+import { Section } from "../components/Section"
+import { Row, Col } from "react-bootstrap"
+import styled from "styled-components"
+
+const Styles = styled.div`
+  background-color: #dc3545;
+  padding: 30px;
+  font-size: 1.2rem;
+`
 
 export const RequestCTF = () => (
-    <React.Fragment>
+  <React.Fragment>
     <SectionPanel>
-        <Section>
-            <SectionTitle>Request a CTF</SectionTitle>
-        </Section>
+      <Section>
+        <SectionTitle>Request a CTF</SectionTitle>
+        <Row className="justify-content-md-center">
+          <Col md={5}>
+            <Styles>
+              The <strong>CTF4Hire</strong> team is currently preparing for
+              NahamCon 2021 and is unable to take more requests at this time.
+            </Styles>
+          </Col>
+        </Row>
+      </Section>
     </SectionPanel>
-</React.Fragment>
+  </React.Fragment>
 )
